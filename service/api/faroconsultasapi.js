b832d707434a1ab1de409d888e95a2ba331a8f37
windowsazure <windowsazure>
2015-03-07 16:11:38 +0000

    Committing update from request /api/scmvfs/service/api/faroconsultasapi.js

--- service/api/faroconsultasapi.js
+++ service/api/faroconsultasapi.js
@@ -1,11 +1,46 @@
-exports.post = function(request, response) {
-    // Use "request.service" to access features of your mobile service, e.g.:
-    //   var tables = request.service.tables;
-    //   var push = request.service.push;
-
-    response.send(statusCodes.OK, { message : 'Hello World!' });
+exports.register = function (api) {      
+       
+        //Trae Requeridos, Asignados y Vancantes agrupados por NodoFuncional o Rol
+        api.get('getconsultasvacantes', getconsultasvacantes);
+             
 };
 
-exports.get = function(request, response) {
-    response.send(statusCodes.OK, { message : 'Hello World!' });
+function getconsultasvacantes(request, response){
+
+        //https://farodosconsultas2015.azure-mobile.net/api/faroconsultasapi/getconsultasvacantes?idorganizacion=10&idnodoorganizacional=2&conrol=0
+        var param1 = parseInt(request.query.idorganizacion);
+        var param2 = parseInt(request.query.idnodoorganizacional);
+        var param3 = parseInt(request.query.conrol);
+
+        var params=[param1,param2,param3];
+        
+        var sql = "EXEC [farodosconsultas2015].[FaroConsultasVacantes] @IdOrganizacion=? ,@IdNodoOrganizacional = ?, @ConRol= ? ";
+        execute(request, sql, params, response);  
+};
+
+    
+function executewithoutparam(request,sql, response){
+       var mssql = request.service.mssql;
+        mssql.query(sql, {
+        success: function(results) {   
+                                   
+                response.send(200, JSON.stringify(results))  
+                
+        }, error: function(err) {
+                response.send(400, { error: err });        
+        }
+        
+       }) 
+};
+
+function execute(request, sql, params, response){        
+       var mssql = request.service.mssql;
+        mssql.query(sql, params, {
+        success: function(results) {                          
+                response.send(200, JSON.stringify(results))                
+        }, 
+        error: function(err) {
+                response.send(400, { error: err });        
+        }        
+       })
 };

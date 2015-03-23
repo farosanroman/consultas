3f3b9d66a8997d93f6d6b9d898dc8ed916b17952
windowsazure <windowsazure>
2015-03-23 23:48:46 +0000

    Committing update from request /api/scmvfs/service/api/faroconsultasapi.js

--- service/api/faroconsultasapi.js
+++ service/api/faroconsultasapi.js
@@ -33,10 +33,7 @@ function getconsultasnodospivoterol(request, response){
         var params=[param1,param2,param3];
         
         var sql = "EXEC [farodosconsultas2015].[FaroConsultasNodosPivoteRol] @idorganizacion=?, @idnodoorganizacional = ?, @roles = ? ";
-              console.log(param1.toString())
-               console.log(param2.toString())
-                console.log(param3)
-              console.log(sql)
+             
               
         execute(request, sql, params, response);  
 };
